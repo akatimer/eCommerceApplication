13be8a5c37ad43e8c06dafe2cc7c37110245c04d
Zhiznevski <w.heisenberggg@gmail.com>
2023-08-08 14:23:58 +0300

    feat: creat login-page basic structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import Form from './components/Form/Form';
+
+export default function App(): ReactElement {
+  return (
+    <div>
+      <Form />
+    </div>
+  );
+}
