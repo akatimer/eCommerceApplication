75ea1669e7e88fc78966bb0d1446e940c73cd8c1
Zhiznevski <w.heisenberggg@gmail.com>
2023-08-08 21:48:31 +0300

    feat: add password validation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,40 @@
-import React, { ReactElement } from 'react';
+import React, { ChangeEvent, ReactElement, useState } from 'react';
 import './Form.css';
+import isPasswordValid from '../../utils/isPasswordValid';
+
+const colors = {
+  validColor: 'green',
+  invalidColor: 'red',
+};
 
 export default function Form(): ReactElement {
+  const [checkMessage, setCheckMessage] = useState('');
+  const [messageColor, setMessageColor] = useState('');
+
+  function passwordHandler(event: ChangeEvent<HTMLInputElement>): void {
+    const password = event.target.value;
+    if (isPasswordValid(password)) {
+      setCheckMessage('password is valid');
+      setMessageColor(colors.validColor);
+    } else {
+      setCheckMessage('password is not valid');
+      setMessageColor(colors.invalidColor);
+    }
+  }
+
   return (
     <div className="form-wrapper">
       <h1>Authorization</h1>
       <form className="auth-form">
-        <input type="email" placeholder="Enter your email..."></input>
-        <input type="password" placeholder="Enter your password..."></input>
+        <input type="email" placeholder="Enter your email..." required></input>
+        <label style={{ color: messageColor }}>{checkMessage}</label>
+        <input
+          type="password"
+          placeholder="Enter your password..."
+          onChange={(e): void => {
+            passwordHandler(e);
+          }}
+        ></input>
         <button className="submit-button" type="submit">
           Sign in
         </button>
