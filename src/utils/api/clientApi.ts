f438e1b349863dd560806321a7e1756021c08738
akatimer <akatimer@gmail.com>
2023-08-13 20:35:44 +0300

    feat: add sdk/clientBuilder / template for Client

---
+++ src/utils/api/clientApi.ts
@@ -0,0 +1,35 @@
+import { ApiRoot, createApiBuilderFromCtpClient } from '@commercetools/platform-sdk';
+import { createClientWithPass, getApiRoot, projectKey } from './clientBuilder';
+
+export const getProject = await getApiRoot()
+  .withProjectKey({ projectKey })
+  // .products()
+  // .get()
+  // .execute();
+  .customers()
+  .get()
+  .execute();
+
+const getApiPassRoot: () => ApiRoot = () => {
+  return createApiBuilderFromCtpClient(
+    createClientWithPass('zhiznevskiyartem@gmail.com', 'qweqweqwe')
+  );
+};
+
+export const getProjectPass = await getApiPassRoot()
+  .withProjectKey({ projectKey })
+  // .products()
+  // .get()
+  // .execute();
+  .me()
+  // .login()
+  // .post({
+  //   body: {
+  //     email: 'test@test.com',
+  //     password: 'cvsy09oq',
+  //   },
+  //   })
+  // // .clientRequest()
+  .get()
+  .execute()
+  .catch(console.error);
