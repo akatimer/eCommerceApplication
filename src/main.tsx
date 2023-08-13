d2dc9ed53c4b9329892a917b0d97ec39867322dd
akatimer <akatimer@gmail.com>
2023-08-13 20:40:57 +0300

    feat: aditional clear

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './pages/App';
-import { getProjectPass } from './utils/api/clientApi';
 
 const root = document.getElementById('root');
 root && ReactDOM.createRoot(root).render(<App />);
-
-console.log(getProjectPass);
