fe30c45152802c2da580bada577dce2d7ff7e4da
Fralou Maksim <fralou@protonmail.com>
2023-08-25 12:28:30 +0300

    fix: solve problem comits

--- src/components/DetailedProduct /DetailedProduct .tsx
+++ src/components/DetailedProduct /DetailedProduct .tsx
@@ -1,32 +1,54 @@
 import './DetailedProduct.css';
-import React from 'react';
-import dressPick from '../../assets/images/dress.png';
+import React, { useState, useEffect } from 'react';
+import { getApiRoot } from '../../utils/api/clientBuilder';
+import { projectKey } from '../../utils/api/clientBuilder';
+import { ProductProjection } from '@commercetools/platform-sdk';
 
 const DetailedProduct: React.FC = () => {
+  const [productData, setProductData] = useState<ProductProjection | null>(null);
+
+  useEffect(() => {
+    const fetchProduct = async (): Promise<void> => {
+      try {
+        const productResponse = await getApiRoot()
+          .withProjectKey({ projectKey })
+          .productProjections()
+          .withKey({ key: 'hoodie_kropka' })
+          .get()
+          .execute();
+
+        setProductData(productResponse.body);
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
+    };
+
+    fetchProduct();
+  }, []);
+
+  if (!productData) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  const image = productData.masterVariant.images && productData.masterVariant.images[0]?.url;
+  const productName = productData.name && productData.name['en-US'];
+  const description = productData.description && productData.description['en-US'];
+  const price =
+    productData.masterVariant.prices && productData.masterVariant.prices[0]?.value.centAmount / 100;
+
   return (
     <div className="prod-container">
       <div className="prod-pic">
-        <img src={dressPick} alt="Dress" />
+        <img src={image} alt={productName} />
       </div>
       <div className="prod-desc-container">
-        <h2 className="prod-title">Linen dress</h2>
+        <h2 className="prod-title">{productName}</h2>
         <div className="desc-wrapper">
           <h4 className="prod-desc-title">Info</h4>
-          <p className="prod-desc">
-            Dress made of 100% linen. Casual feminine dress with puffed sleeves. The length of the
-            dress is midi, below the knee. Button closure on the back, neck without trimming.
-            Natural coconut buttons. Free cut bodice with darts, moderately puffy skirt, puffy
-            sleeves above the elbow, gathered on the elastic band at the bottom. Basic dress will
-            suit girls of any age, as well as pregnant women. Linen dress looks expensive and noble,
-            will become the basis of any fashionable image. 100% softened linen with a crumpled
-            effect perfectly transmits air and absorbs moisture, it is a hypoallergenic material of
-            the highest quality. The manufacturer of the fabric is Orsha Flax Factory (Belarus). The
-            dress will fit well for a height of 155-170 cm. Model&apos;s height is 171 cm, chest
-            circumference is 74 cm, dress size on the photo is 38.
-          </p>
+          <p className="prod-desc">{description}</p>
         </div>
         <div className="price">
-          $ <span className="cost">1</span>
+          $ <span className="cost">{price}</span>
         </div>
       </div>
     </div>
