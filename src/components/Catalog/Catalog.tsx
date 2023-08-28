506668210a982eb74a442d8d84397a3cb654f406
Zhiznevski <w.heisenberggg@gmail.com>
2023-08-28 15:03:32 +0300

    feat: create search component with functional

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -5,36 +5,58 @@ import { getProducts } from '../../utils/api/clientApi';
 import ProductCard from '../ProductCard/ProductCard';
 import SortDropdown from '../SortDropdown/SortDropdown';
 import './Catalog.css';
+import Search from '../Search/Search';
 
 const Catalog: React.FC = () => {
   const [products, setProducts] = useState<ProductProjection[]>();
   const [sorting, setSorting] = useState('');
+  const [searchValue, setSearchValue] = useState('');
+  const [showModal, setShowModal] = useState(false);
+  const [notFound, setNotFound] = useState(false);
 
   const handleChange = (event: SelectChangeEvent): void => {
     setSorting(event.target.value);
   };
+  const searchHandler = (event: React.SyntheticEvent): void => {
+    const target = event.target as HTMLInputElement;
+    setSearchValue(target.value);
+    target.value.length < 4 && target.value.length > 0 ? setShowModal(true) : setShowModal(false);
+  };
+
   useEffect(() => {
     getProducts({
       queryArgs: {
         sort: sorting ? sorting : 'price asc',
+        'text.en-US': searchValue.length > 3 ? searchValue : '',
+        fuzzy: true,
       },
     })
       .then((response) => {
-        if (response) {
+        if (response?.body.count) {
           setProducts(response.body.results);
+          setNotFound(false);
+        } else {
+          setProducts([]);
+          setNotFound(true);
         }
       })
       .catch(console.error);
-  }, [sorting]);
+  }, [sorting, searchValue]);
 
   if (!products) {
     return <div className="loading">Loading...</div>;
   }
   return (
-    <>
+    <div className="catalog">
       <div className="control-block">
+        <Search searchHandler={searchHandler} showModal={showModal} />
         <SortDropdown handleChange={handleChange} sorting={sorting} />
       </div>
+      {notFound && (
+        <div className="not-found">
+          Sorry, we couldn&apos;t find any matching result for your query.
+        </div>
+      )}
       <Grid container alignItems={'center'} spacing={6} sx={{ margin: 'auto' }}>
         {products &&
           products.map((product) => (
@@ -43,7 +65,7 @@ const Catalog: React.FC = () => {
             </Grid>
           ))}
       </Grid>
-    </>
+    </div>
   );
 };
 
