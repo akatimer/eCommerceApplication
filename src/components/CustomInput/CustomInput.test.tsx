776f65f9b157c5421c4cfae9eaa40dfcf4f3d3f7
akatimer <akatimer@gmail.com>
2023-09-11 18:33:24 +0300

    feat: add aditional tests

--- src/components/CustomInput/CustomInput.test.tsx
+++ src/components/CustomInput/CustomInput.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 import CustomInput from './CustomInput';
 import React from 'react';
@@ -21,4 +21,52 @@ describe('PostalCode', () => {
     );
     expect(screen.getAllByDisplayValue(/11111/i)).toBeDefined();
   });
+});
+
+test('Should test valid input color', () => {
+  const correct = 'Valid Input';
+  const correctCollor = 'rgb(0, 128, 0)';
+
+  const { getByPlaceholderText, getByText } = render(
+    <CustomInput
+      validColor={correctCollor}
+      invalidColor={'rgb(255,0,0)'}
+      validMessage={correct}
+      invalidMessage={'Invalid Input'}
+      validator={(value): boolean => value.length >= 5}
+      placeholder="Test Input"
+      type="text"
+      onChange={(): void => {}}
+    />
+  );
+
+  const inputElement = getByPlaceholderText('Test Input');
+
+  fireEvent.change(inputElement, { target: { value: 'validValue' } });
+
+  expect(getByText(correct)).toHaveStyle(`color: ${correctCollor}`);
+});
+
+test('Should test invalid input color', () => {
+  const incorrectColor = 'rgb(255,0,0)';
+  const invalidMessage = 'Invalid Input';
+
+  const { getByPlaceholderText, getByText } = render(
+    <CustomInput
+      validColor={'rgb(0, 128, 0)'}
+      invalidColor={incorrectColor}
+      validMessage={'correct'}
+      invalidMessage={invalidMessage}
+      validator={(value): boolean => value.length >= 5}
+      placeholder="Test Input"
+      type="text"
+      onChange={(): void => {}}
+    />
+  );
+
+  const inputElement = getByPlaceholderText('Test Input');
+
+  fireEvent.change(inputElement, { target: { value: 'inv' } });
+
+  expect(getByText(invalidMessage)).toHaveStyle(`color: ${incorrectColor}`);
 });
